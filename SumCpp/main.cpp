f9bf23b0dfe3b451d5b9c64b4cb821cbd7f4bb4b
Hazem Elbaz <hazemelbaz@hazems-macbook-pro.local>
2019-03-02 21:06:10 +0200

    this 1st modification on the file here we create the app for sumation

--- SumCpp/main.cpp
+++ SumCpp/main.cpp
@@ -7,9 +7,23 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World! .. This Sum Cpp App !!!\n";
+    
+    int num1=0, num2=0, sum=0;
+    
+    cout << "Enter 1st num: ";
+    cin >> num1;
+    
+    cout << "Enter 2nd num: ";
+    cin >> num2;
+    
+    cout << "Sum = ";
+    sum = num1 + num2 ;
+    cout << sum;
+    
     return 0;
 }
