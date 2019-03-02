cb741c830fe048be5e5ed5ab169f4cba71830bf9
Hazem Elbaz <hazemelbaz@hazems-macbook-pro.local>
2019-03-02 21:01:33 +0200

    Initial Commit

---
+++ SumCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SumCpp
+//
+//  Created by Hazem Elbaz on 3/2/19.
+//  Copyright © 2019 Hazem Elbaz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
